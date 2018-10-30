3c7ebf4895b70d8e06d4ab54e946f9db47fc306b
William B McAllister <blakemcallister77@gmail.com>
2018-10-30 19:06:29 -0400

    pg 165

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ExceptionHandlingExercise
 {
@@ -10,6 +11,33 @@ namespace ExceptionHandlingExercise
 	{
 		static void Main(string[] args)
 		{
+			bool validAnswer = false;
+			int userAge = 0;
+			try
+			{
+				while (!validAnswer)
+				{
+					Console.WriteLine("Hello, How old are you or will you be this year?");
+					validAnswer = int.TryParse(Console.ReadLine(), out userAge);
+					if (!validAnswer) Console.WriteLine("Please enter whole number only! No decimals. Thank you.");
+
+				}
+				if (userAge <= 0)
+				{
+					throw new Exception();
+				}
+				int birthYear = 2018 - userAge;
+
+				Console.WriteLine("You were born in " + birthYear + ".");
+				Console.ReadLine();
+			}
+			catch (Exception)
+			{
+				Console.WriteLine("Please enter positive whole number digits only, no decimal numbers, zero, or negative numbers.");
+				Console.ReadLine();
+				return;
+			}
+
 		}
 	}
 }
